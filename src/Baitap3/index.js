c307ba743f493d21734014c5cb697f053ad9d452
Tu Vo <votu1703@gmail.com>
2020-09-02 04:46:01 -0700

    ok

---
+++ src/Baitap3/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./header";
+import Carousel from "./carousel";
+import ListSmartphone from "./listSmartphone";
+import ListLaptop from "./listLaptop";
+import Promotion from "./promotion";
+
+export default function Baitap3(){
+    return (
+        <>
+        <Header/>
+        <Carousel/>
+        <ListSmartphone/>
+        <ListLaptop/>
+        <Promotion/>
+        </>
+
+    );
+}
